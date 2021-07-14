387e862320de2d6d70e6a7a3b3fc238236ce5c78
rohithk <rohithk246@gmail.com>
2021-07-14 19:10:53 +0530

    Completed Lab4

--- Lab4/Code/src/Cache.java
+++ Lab4/Code/src/Cache.java
@@ -2,8 +2,15 @@ public class Cache {
 	private static int MAX_CAPACITY = 0;
 	
 	// Implement static block
+	static void setMaxCapacity(int x) {
+		MAX_CAPACITY = x;
+	}
 
 	static int getMaxCapacity() {
+		if(MAX_CAPACITY==0) {
+			System.out.println("Enter Max Capacity : ");
+			setMaxCapacity(Console.readInt());
+		}
 		System.out.println("Returning MAX_CAPACITY");
 		return MAX_CAPACITY;
 	}
