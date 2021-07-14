f2b54b712c46399c1daa0b22163d8dbcbab7084b
rohithk <rohithk246@gmail.com>
2021-07-14 18:56:29 +0530

    Completed Lab3

--- Lab3/Code/src/Address.java
+++ Lab3/Code/src/Address.java
@@ -1,7 +1,38 @@
 public class Address {
+	
+	private String addr1, addr2, city, pin;
 
+	public String getAddr1() {
+		return addr1;
+	}
 
-//	Complete the code by creating variables and methods
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
 
+	public String getAddr2() {
+		return addr2;
+	}
 
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+
+	
 }
--- Lab3/Code/src/Employee.java
+++ Lab3/Code/src/Employee.java
@@ -1,5 +1,24 @@
 public class Employee {
 
-// Identity variables and methods. Create association with Address
-
+	private String empId, name;
+	private Address address;
+	
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 }
