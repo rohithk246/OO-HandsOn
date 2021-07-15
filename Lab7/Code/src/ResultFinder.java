a2772dff0e96ed9eb073199b9621e4d02487bdb9
rohithk <rohithk246@gmail.com>
2021-07-15 11:23:45 +0530

    Completed Lab7

--- Lab7/Code/src/ResultFinder.java
+++ Lab7/Code/src/ResultFinder.java
@@ -1,25 +1,48 @@
 public class ResultFinder {
+	
 	private int m1;
 	private int m2;
 	private int m3;
 	
+	public int getM1() {
+		return m1;
+	}
+	public void setM1(int m1) {
+		this.m1 = m1;
+	}
+	public int getM2() {
+		return m2;
+	}
+	public void setM2(int m2) {
+		this.m2 = m2;
+	}
+	public int getM3() {
+		return m3;
+	}
+	public void setM3(int m3) {
+		this.m3 = m3;
+	}
 	// Create setter and getter methods 
 
 	public void displayMarks() {
-
-
-	
+		System.out.println("Marks in subject 1  : " + m1);
+		System.out.println("Marks in subject 2 : " + m2);
+		System.out.println("Marks in subject 3 : " + m3);
 	}
 	public int getTotal() {
-//		complete the logic
+		return m1+m2+m3;
 	}
 	public double getAverage() {
-//		complete the logic
+		return getTotal()/3.0;
 	}
 	
 	public String getResult() {
-
-//		complete the logic
-
+		return "Subject 1 - " + getGrade(m1) + 
+				", Subject 2 - " + getGrade(m2) + 
+				", Subject 3 - " + getGrade(m3);
+	}
+	public String getGrade(int marks) {
+		String res = "FFFFEDCBASS";
+		return "" + res.charAt(marks/10);
 	}
 }
--- Lab7/Code/src/TestMain.java
+++ Lab7/Code/src/TestMain.java
@@ -4,15 +4,17 @@ public class TestMain {
 			System.out.println("Please enter 3 marks!");
 			System.exit(0);
 		}
-		int m1 = 0;
-		int m2 = 0;
-		int m3 = 0;
+		int m1 = Integer.parseInt(args[0]);
+		int m2 = Integer.parseInt(args[1]);
+		int m3 = Integer.parseInt(args[2]);
 		
 
 		
 		ResultFinder finder = new ResultFinder();
 		
-
+		finder.setM1(m1);
+		finder.setM2(m2);
+		finder.setM3(m3);
 		
 		finder.displayMarks();
 		
